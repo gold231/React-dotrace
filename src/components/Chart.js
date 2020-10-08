7e4799eb239cd7692a7eade9adf0a0c1fb149120
Gold813 <goldendragon0318@gmail.com>
2020-10-08 04:08:23 -0400

    chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,39 +1,69 @@
 import React, { Component } from 'react';
 import { Line } from 'react-chartjs-2';
+import Papa from 'papaparse';
 
 class Chart extends Component {
+  
   constructor(props) {
     super(props);
     this.state = {
-      chartData: {
-        labels: [0, 1, 2, 3, 4, 5],
-        datasets: [
-          {
-            label: "Red's Token",
-            data: [
-              617594, 181045, 153060, 106519, 105162, 95072
-            ],
-            fill: false,
-            borderColor: "red",
-            pointBorderColor: "transparent",
-            pointHoverBackgroundColor: "red"
-          },
-          {
-            label: "Blue's Token",
-            data: [
-              123345, 378422, 234563, 678234, 133464, 785245
-            ],
-            fill: false,
-            borderColor: "Blue",
-            pointBorderColor: "transparent",
-            pointHoverBackgroundColor: "Blue"
-          }
-        ]
-      }      
-    }
+      xlabels: [],
+      redToken: [],
+      blueToken: []
+    };
+    this.GetData = this.GetData.bind(this);
+    this.fetchCsv = this.fetchCsv.bind(this);
+  }
+
+  async GetData(artist) {
+    const data = Papa.parse(await this.fetchCsv());
+    let xlabels = [];
+    let redToken = [];
+    let blueToken = [];
+    for(let i=1; i<100;i++) {
+      xlabels.push(data.data[i][0]);
+      redToken.push(data.data[i][1]);
+      blueToken.push(data.data[i][2]);
+    }  
+    this.setState({xlabels: xlabels, redToken: redToken, blueToken: blueToken});
+    return data;
   }
   
-  render () {
+  async fetchCsv() {
+    const response = await fetch('data/Scenario2.csv');
+    const reader = response.body.getReader();
+    const result = await reader.read();
+    const decoder = new TextDecoder('utf-8');
+    const csv = await decoder.decode(result.value);
+    return csv;
+  }
+
+  render () {    
+    this.GetData();
+    console.log("xlabels",this.state.xlabels);
+
+    let chartData = {
+      labels: this.state.xlabels,
+      datasets: [
+        {
+          label: "Red's Token",
+          data: this.state.redToken,
+          fill: false,
+          borderColor: "red",
+          pointBorderColor: "transparent",
+          pointHoverBackgroundColor: "red"
+        },
+        {
+          label: "Blue's Token",
+          data: this.state.blueToken,
+          fill: false,
+          borderColor: "Blue",
+          pointBorderColor: "transparent",
+          pointHoverBackgroundColor: "Blue"
+        }
+      ]
+    }  
+
     let options = {
       legend: {
         display: true
@@ -70,15 +100,17 @@ class Chart extends Component {
         }
       }
     };
+
     return (
         <>
           <div className="chart">
             <Line 
-              data={this.state.chartData}
+              data={chartData}
               options={options}
               width={800}
               height={500}
             />
+            
           </div>
         </>
     )
